84c1a306bd03e4bd2c3310c45ec46d4d72b55a9a
DYA-Code <entrydia7691@gmail.com>
2022-09-07 23:19:50 +0900

    json

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,34 @@
 import express from 'express'
 import cors from 'cors'
+import fs from 'fs'
 const app = express()
 const port = process.env.PORT || 3001
+const data = require('../config/data.json')
 
 app.use(cors())
-
-// import { usrTable } from '../data/usr.json'
 
 app.get('/', (_, res) => {
     res.send('Backend Server')
 })
+
+app.get("/new/:title/:content", (req, res) => {
+
+    res.header("Access-Control-Allow-Origin", "*")
+
+    const title = req.params.title
+    const content = req.params.content
+
+    let newblog = {
+        "key": data.blogs.length,
+        "title": title,
+        "content": content,
+    }
+
+    data.blogs.push({ "key": data.blogs.length, "title": title, "content": content })
+
+    fs.writeFileSync('./config/data.json', JSON.stringify(data))
+
+    res.send(`title: ${title} / content: ${content}`)
+});
 
 app.listen(port, () => { console.log(`http://localhost:${port}`) })
